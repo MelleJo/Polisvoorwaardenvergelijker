615c4e975ce96fd843e5770ff91cdf2602d454b2
MJ <mellejorna@gmail.com>
2024-03-14 18:55:30 +0100

    test

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import streamlit as st
+import os
+import time
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.chains import AnalyzeDocumentChain
-from langchain.prompts import ChatPromptTemplate
-from langchain.output_parsers import StrOutputParser
+from langchain_community.callbacks import get_openai_callback
+from langchain.chains.question_answering import load_qa_chain
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
 def extract_text_from_pdf(file):
     reader = PdfReader(file)
